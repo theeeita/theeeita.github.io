ed2f9173f5b98e58234c7b9aba622ebade04343a
theeeita <51116054+theeeita@users.noreply.github.com>
2019-10-28 01:40:00 +0300

    fix adblock bug

--- bakery/js/common.js
+++ bakery/js/common.js
@@ -82,4 +82,4 @@ $priceCards.on("click", function () {
   shuffleRight($clickedSiblings, { start: -15, offset: 15 }, $clickedSiblings.length, { start: 0.95, scaling: 0.05 }, 992);
 });
 
-$(".teaser-image-wrapper img").attr("style", "display: block !important");
+$(".cake-image-wrapper img").attr("style", "display: block !important");
